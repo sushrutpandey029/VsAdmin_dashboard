1a2b7aa0ac16100c3262681aa2787997bf46e0c6
sushrutpandey <sushrutpandey1@gmail.com>
2023-02-10 09:58:42 +0530

    first commit

--- src/controller/patientController.js
+++ src/controller/patientController.js
@@ -462,8 +462,14 @@ const createPatientnew = async function (req, res) {
             to: body.email, // list of receivers
             subject: "Registration Confirmed VS", // Subject line
             text: "Welcome to Voice Simulation", // plain text body
-            html: `<b>Hi ${body.patientFullName}</b><br><b>Welcome to Voice Simulation</b><br><p>Your registration was successful. Thank you for joining our service!</p><<br> Best Regards <br>Voice Simulation <br> Head Office <br>Thank You `, // html body
-          });
+             html: `<b>Dear ${body.patientFullName}</b><br><b>Welcome to Voice Simulation</b><br>
+            <p>Your registration was successful. Thank you for joining our service!</p>
+            <p>For any query do reply to this email</p><br>
+            <p><a href="#"><img src="https://techmonitor.ai/wp-content/uploads/sites/4/2016/06/what-is-URL.jpg" height="200px" width="200px"/></a></p>
+            <p><a href="#"><button>Download Application</button></a></p>
+            <p>Best Regards</p>
+            <p>Head Office</p><p>Voice Simulation</p><p>New Ashok Nagar Delhi 110096 Near Metro Station Noida sector-18</p><p><b>Thank You</b></p> `, // html body
+      });
       
           console.log("new user", info.messageId);
           console.log("To", info);
